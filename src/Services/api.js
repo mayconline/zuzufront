94eed8c1cffadf9fdaf150123c7bfaabc06dce28
mayconline <mayconline.ti@gmail.com>
2019-07-19 20:53:15 -0300

    ajuste

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-   // baseURL:'https://zuzu-back.herokuapp.com/'
-   baseURL:'http://localhost:3000/'
+    baseURL:'https://zuzu-back.herokuapp.com/'
+  // baseURL:'http://localhost:3000/'
    
 });
 
