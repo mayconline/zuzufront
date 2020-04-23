15ff0c262eada1ed1a6b42bb29b9e8f3b9e0f63b
mayconline <mayconline.ti@gmail.com>
2020-04-23 13:41:35 -0300

    fixing upload image with tag space

--- src/Components/Upload/styled.js
+++ src/Components/Upload/styled.js
@@ -1,57 +1,54 @@
-import styled , {css} from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-   
-    display:flex;
-    justify-content:center;
-    align-items:center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Content = styled.div`
-    width:100%;
-    min-width:360px;
-    margin:30px;
-    background:#fff;
-    border-radius:4px;
-    padding:20px;
+  width: 100%;
+  min-width: 360px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 0 10px;
 
-    .img-prev{
-        width:100%;
-        min-width:360px;
-       
-    }
+  .img-prev {
+    max-width: 400px;
+    min-width: 360px;
+    padding: 0 10px;
+  }
 `;
 
-
 export const DropContainer = styled.div.attrs({
-    className:'dropzone'
+  className: 'dropzone',
 })`
-    border:1px dashed #ddd;
-    border-radius:4px;
-    cursor:pointer;
-    transition: height 0.2s ease;
-    ${props => props.isDragActive && dragActive};
-    ${props => props.isDragReject && dragReject};
+  border: 1px dashed #ddd;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: height 0.2s ease;
+  ${props => props.isDragActive && dragActive};
+  ${props => props.isDragReject && dragReject};
 `;
 
 const dragActive = css`
-    border-color:#78e5d5;
+  border-color: #78e5d5;
 `;
 
-const dragReject= css`
-    border-color:#e57878;
+const dragReject = css`
+  border-color: #e57878;
 `;
 
-const msgColor ={
-    default:'#999',
-    error: '#e57878',
-    success:'#78e5d5'
+const msgColor = {
+  default: '#999',
+  error: '#e57878',
+  success: '#78e5d5',
 };
 
 export const UploadMessage = styled.p`
-    display:flex;
-    color:${props => msgColor[props.type || 'default']};
-    justify-content:center;
-    align-items:center;
-    padding:15px 0;
+  display: flex;
+  color: ${props => msgColor[props.type || 'default']};
+  justify-content: center;
+  align-items: center;
+  padding: 15px 0;
 `;
